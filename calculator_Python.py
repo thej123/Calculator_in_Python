c50ddf69154e9dfcb995ea8bad61999bc4f94a29
thej <thej.kodiyat@rackspace.com>
2018-06-21 16:48:35 -0500

    Calculator in Python

---
+++ calculator_Python.py
@@ -0,0 +1,127 @@
+# coding=utf-8
+
+from Tkinter import *
+import math
+
+class Calculator:
+    def find_and_replace(self):
+        """ Replace x with * and ÷ with / """
+
+        self.expression = self.e.get()
+        self.newtext = self.expression.replace(self.newdiv, '/')
+        self.newtext = self.newtext.replace('x','*')
+    
+    def equals(self):
+        """ when the equal sign is pressed """
+
+        self.find_and_replace()
+        try:
+            self.value = eval(self.newtext) #evaluate the expression using the eval function
+        except SyntaxError or NameError:
+            self.e.delete(0,END)
+            self.e.insert(0,'Invalid Input!')
+        else:
+            self.e.delete(0, END)
+            self.e.insert(0,self.value)
+
+    def square_root(self):
+        """ square root method """
+
+        self.find_and_replace()
+        try:
+            self.value = eval(self.newtext) #evaluate the expression using the eval function
+        except SyntaxError or NameError:
+            self.e.delete(0,END)
+            self.e.insert(0,'Invalid Input!')
+        else:
+            self.sqrt_val = math.sqrt(self.value)
+            self.e.delete(0, END)
+            self.e.insert(0,self.value)
+
+    def square(self):
+        """ square method """
+
+        self.find_and_replace()
+        try:
+            self.value = eval(self.newtext) #evaluate the expression using the eval function
+        except SyntaxError or NameError:
+            self.e.delete(0,END)
+            self.e.insert(0,'Invalid Input!')
+        else:
+            self.square_val = math.pow(self.value,2)
+            self.e.delete(0, END)
+            self.e.insert(0,self.value)
+    
+    def clear_all(self):
+        """ when clear button is pressed, clears the text input area """
+        self.e.delete(0, END)
+    
+    def clear_one(self):
+        """ when clear one character button is pressed, delete the latest character in the input area """
+
+        self.text = self.e.get()[:-1]
+        self.e.delete(0, END)
+        self.e.insert(0, self.text)
+    
+    def action(self,argi):
+        """ pressed button's value is inserted into the end of the text area """
+        self.e.insert(END,argi)
+
+    def __init__(self, master):
+        """ Constructor method """
+
+        master.title('Calculator')
+        master.geometry()
+        self.e = Entry(master)
+        self.e.grid(row=0,column=0,columnspan=6,pady=3)
+        self.e.focus_set() #Sets focus on the input text area
+
+        self.div = '÷'
+        self.newdiv = self.div.decode('utf-8')
+
+        #Generating Buttons
+        Button(master, text="=", width=10, command=lambda:self.equals()).grid(row=4, column=4, columnspan=2)
+        Button(master, text="AC", width=3, command=lambda:self.clear_all()).grid(row=1, column=4)
+        Button(master, text="C", width=3, command=lambda:self.clear_one()).grid(row=1, column=5)
+        Button(master, text="+", width=3, command=lambda:self.clear_one()).grid(row=1, column=5)
+        Button(master,text="x",width=3,command=lambda:self.action('x')).grid(row=2, column=3)
+        Button(master,text="-",width=3,command=lambda:self.action('-')).grid(row=3, column=3)
+        Button(master,text="÷",width=3,command=lambda:self.action(self.newdiv)).grid(row=1, column=3) 
+        Button(master,text="%",width=3,command=lambda:self.action('%')).grid(row=4, column=2)
+        Button(master,text="7",width=3,command=lambda:self.action('7')).grid(row=1, column=0)
+        Button(master,text="8",width=3,command=lambda:self.action(8)).grid(row=1, column=1)
+        Button(master,text="9",width=3,command=lambda:self.action(9)).grid(row=1, column=2)
+        Button(master,text="4",width=3,command=lambda:self.action(4)).grid(row=2, column=0)
+        Button(master,text="5",width=3,command=lambda:self.action(5)).grid(row=2, column=1)
+        Button(master,text="6",width=3,command=lambda:self.action(6)).grid(row=2, column=2)
+        Button(master,text="1",width=3,command=lambda:self.action(1)).grid(row=3, column=0)
+        Button(master,text="2",width=3,command=lambda:self.action(2)).grid(row=3, column=1)
+        Button(master,text="3",width=3,command=lambda:self.action(3)).grid(row=3, column=2)
+        Button(master,text="0",width=3,command=lambda:self.action(0)).grid(row=4, column=0)
+        Button(master,text=".",width=3,command=lambda:self.action('.')).grid(row=4, column=1)
+        Button(master,text="(",width=3,command=lambda:self.action('(')).grid(row=2, column=4)
+        Button(master,text=")",width=3,command=lambda:self.action(')')).grid(row=2, column=5)
+        Button(master,text="√",width=3,command=lambda:self.square_root()).grid(row=3, column=4)
+        Button(master,text="x²",width=3,command=lambda:self.square()).grid(row=3, column=5)
+
+#Main
+root = Tk()
+obj=Calculator(root) #object instantiated
+root.mainloop()
+
+
+#         *** uWSGI is running in multiple interpreter mode ***
+# spawned uWSGI master process (pid: 4457)
+# spawned uWSGI worker 1 (pid: 4459, cores: 8)
+# Python auto-reloader enabled
+# 2018-06-07 17:15:23 [uWSGIWorker1Core0] - flashmob.api.request - INFO - Get headers: {'X-ROLES': 'Racker,encore_mass_comms_staging,lefty_ticket_admin,encore_mass_comms_flashmob_staging', 'X-MAP-ROLES': 'eyJyZXBvc2UvZG9tYWluL3JvbGVzIjpbIlJhY2tlciIsImVuY29yZV9tYXNzX2NvbW1zX3N0YWdpbmciLCJsZWZ0eV90aWNrZXRfYWRtaW4iLCJlbmNvcmVfbWFzc19jb21tc19mbGFzaG1vYl9zdGFnaW5nIl19', 'X-PP-USER': 'AAAt4HjZHKN4AVexaZftZBGX-QGIg5ejL4CxJHR1dOFuE_4k3VNIgu6JLOvhS3vFVfBRmkkDkXVCFZdtAePrnDPvMtmCY9_Eymo8PiLoVf6U5LiKlRZYCVCb;q=0.1, 192.168.33.1;q=0.4, flashmob_stage', 'X-TOKEN-EXPIRES': 'Fri, 08 Jun 2018 09:02:20 GMT', 'X-AUTHORIZATION': 'Proxy', 'HOST': 'localhost:8001', 'X-AUTH-TOKEN-KEY': 'IDENTITY:V2:TOKEN:AAAt4HjZHKN4AVexaZftZBGX-QGIg5ejL4CxJHR1dOFuE_4k3VNIgu6JLOvhS3vFVfBRmkkDkXVCFZdtAePrnDPvMtmCY9_Eymo8PiLoVf6U5LiKlRZYCVCb', 'VIA': '1.1 localhost:8080 (Repose/8.8.3.0)', 'X-AUTHENTICATED-BY': 'PASSWORD', 'X-USER-ID': 'flashmob_stage', 'X-FORWARDED-FOR': '192.168.33.1', 'CONNECTION': 'Keep-Alive', 'X-AUTH-TOKEN': 'AAAt4HjZHKN4AVexaZftZBGX-QGIg5ejL4CxJHR1dOFuE_4k3VNIgu6JLOvhS3vFVfBRmkkDkXVCFZdtAePrnDPvMtmCY9_Eymo8PiLoVf6U5LiKlRZYCVCb', 'X-USER-NAME': 'flashmob_stage', 'X-PP-GROUPS': 'X-Auth-Token;q=0.1, ipv4-match-all;q=0.4', 'ACCEPT': 'application/json'}
+# 2018-06-07 17:15:23 [uWSGIWorker1Core0] - flashmob.api.request - INFO - REQUEST: flashmob_stage|GET /api/notifications|None
+# 2018-06-07 17:15:23 [uWSGIWorker1Core0] - flashmob.api.request - INFO - RESPONSE: 200 OK|['{"prev_page": null, "total_items": 0, "items": [], "next_page": null, "total_pages": 0}']|45.696ms
+# [pid: 4459|app: 0|req: 1/1] 127.0.0.1 () {50 vars in 1388 bytes} [Thu Jun  7 17:15:23 2018] GET /api/notifications => generated 87 bytes in 47 msecs (HTTP/1.1 200) 2 headers in 108 bytes (1 switches on core 0)
+
+
+# Python auto-reloader enabled
+# 2018-06-07 17:18:20 [uWSGIWorker1Core0] - flashmob.api.request - INFO - Get headers: {'X-ROLES': 'Racker,encore_mass_comms_staging,lefty_ticket_admin,encore_mass_comms_flashmob_staging', 'CONNECTION': 'Keep-Alive', 'X-AUTH-TOKEN': 'AAAt4HjZHKN4AVexaZftZBGX-QGIg5ejL4CxJHR1dOFuE_4k3VNIgu6JLOvhS3vFVfBRmkkDkXVCFZdtAePrnDPvMtmCY9_Eymo8PiLoVf6U5LiKlRZYCVCb', 'X-AUTH-TOKEN-KEY': 'IDENTITY:V2:TOKEN:AAAt4HjZHKN4AVexaZftZBGX-QGIg5ejL4CxJHR1dOFuE_4k3VNIgu6JLOvhS3vFVfBRmkkDkXVCFZdtAePrnDPvMtmCY9_Eymo8PiLoVf6U5LiKlRZYCVCb', 'X-PP-GROUPS': 'X-Auth-Token;q=0.1, ipv4-match-all;q=0.4', 'X-AUTHENTICATED-BY': 'PASSWORD', 'X-USER-NAME': 'flashmob_stage', 'X-MAP-ROLES': 'eyJyZXBvc2UvZG9tYWluL3JvbGVzIjpbIlJhY2tlciIsImVuY29yZV9tYXNzX2NvbW1zX3N0YWdpbmciLCJsZWZ0eV90aWNrZXRfYWRtaW4iLCJlbmNvcmVfbWFzc19jb21tc19mbGFzaG1vYl9zdGFnaW5nIl19', 'X-TOKEN-EXPIRES': 'Fri, 08 Jun 2018 09:02:20 GMT', 'VIA': '1.1 localhost:8080 (Repose/8.8.3.0)', 'X-PP-USER': 'AAAt4HjZHKN4AVexaZftZBGX-QGIg5ejL4CxJHR1dOFuE_4k3VNIgu6JLOvhS3vFVfBRmkkDkXVCFZdtAePrnDPvMtmCY9_Eymo8PiLoVf6U5LiKlRZYCVCb;q=0.1, 192.168.33.1;q=0.4, flashmob_stage', 'X-FORWARDED-FOR': '192.168.33.1', 'HOST': 'localhost:8001', 'ACCEPT': 'application/json', 'X-USER-ID': 'flashmob_stage', 'X-AUTHORIZATION': 'Proxy'}
+# 2018-06-07 17:18:20 [uWSGIWorker1Core0] - flashmob.api.request - INFO - REQUEST: flashmob_stage|GET /api/notifications|None
+# 2018-06-07 17:18:20 [uWSGIWorker1Core0] - flashmob.api.request - INFO - RESPONSE: 200 OK|['{"items": [], "total_pages": 0, "total_items": 0, "prev_page": null, "next_page": null}']|41.085ms
+# [pid: 4495|app: 0|req: 1/1] 127.0.0.1 () {50 vars in 1388 bytes} [Thu Jun  7 17:18:20 2018] GET /api/notifications => generated 87 bytes in 45 msecs (HTTP/1.1 200) 2 headers in 108 bytes (1 switches on core 0)
